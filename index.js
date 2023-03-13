404964e36f9b54caa2b0393c3e06ca34fe998128
ochieng <cindycynthia387@gmail.com>
2023-03-13 23:16:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(Feet){
+  if (Feet<400){
+    return 'This one is on me!'
+  }
+    else if(Feet>2000 && Feet<2500){
+      return 'I will gladly take your thirty bucks.'
+      }
+      else if (Feet>2500){
+        return 'No can do.'
+      }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city ==="NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
+  switch (Tip){
+  case 'generous':
+    return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+      default:
+        return 'Bye.';
+  }
+
+
   // Write your code here!
 }
